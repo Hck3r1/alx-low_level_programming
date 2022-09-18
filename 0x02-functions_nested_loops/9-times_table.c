bf0dad4e9adcbe0814f168e4baea3480420ae669
Idima Oluwaferanmi <feranmiidima82@gmail.com>
2022-09-18 14:41:04 -0700

    fibonacci

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- *times_table - Prints the 9 times table, starting with 0.
+ * times_table - Prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
@@ -25,7 +25,6 @@ void times_table(void)
 
 			_putchar((prod % 10) + '0');
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
-
